8801b73d464fa5f9c531c06b5e122210bd2dd478
MW-Limah <cdrmw16@gmail.com>
2026-09-13 01:25:34 +0100

    Permito acesso à API via meu portifólio

--- src/main.py
+++ src/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:5000",
+        "https://delimadev.vercel.app/",
         "https://reviewsense-0e26.onrender.com",
     ],
     allow_credentials=True,
